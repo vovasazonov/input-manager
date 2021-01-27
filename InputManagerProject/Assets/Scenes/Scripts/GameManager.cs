f755a2e59ed84b21e4e04a53e4f94a9ce62c046e
Vladimir <48253536+vovasazonov@users.noreply.github.com>
2021-01-27 15:00:58 +0300

    add: GameManager

---
+++ InputManagerProject/Assets/Scenes/Scripts/GameManager.cs
@@ -0,0 +1,19 @@
+﻿using Inputs;
+using UnityEngine;
+
+public sealed class GameManager : MonoBehaviour
+{
+    [SerializeField] private UnityDpad _dpad;
+
+    private IInputManager _inputManager;
+
+    private void Awake()
+    {
+        _inputManager = new InputManager(_dpad);
+    }
+
+    private void Update()
+    {
+        Debug.Log($"X: {_inputManager.Dpad.Position.X} Y: {_inputManager.Dpad.Position.Y}");
+    }
+}
