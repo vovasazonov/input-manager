8e3ec883f3e209001abd2a36f951b8393c159dd2
Vladimir <48253536+vovasazonov@users.noreply.github.com>
2021-02-18 17:09:23 +0300

    fix: on open dialog windows select default selectable object

--- InputManagerProject/Assets/Scripts/Samples/UI/DialogView.cs
+++ InputManagerProject/Assets/Scripts/Samples/UI/DialogView.cs
@@ -1,12 +1,16 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 namespace Samples.UI
 {
     public sealed class DialogView : MonoBehaviour, IDialogView
     {
+        [SerializeField] private Selectable _selectableOnStart;
+        
         public void Open()
         {
             gameObject.SetActive(true);
+            _selectableOnStart.Select();
         }
 
         public void Close()
